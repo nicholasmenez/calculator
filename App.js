cac7cb6bfe1929b4e22b86b332e08d3b745e26a2
Nicholas Menezes <nicholas.demenezes90@gmail.com>
2019-10-19 19:33:22 +0200

    First commit

---
+++ App.js
@@ -0,0 +1,15 @@
+const calculator = document.querySelector('calculator')
+const keys = calculator.querySelector('.calculator_keys')
+
+keys.addEventListener('click', e =>{
+    if(e.target.matches('button')){
+        
+    }
+});
+
+const key = e.target
+const action = key.dataset.action
+
+if (!action) {
+    console.log('number key!')
+  }
